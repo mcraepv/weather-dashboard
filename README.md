ec1c9d2d3275432729ab337d6d8841b31d0be1a8
mcraepv <61661042+mcraepv@users.noreply.github.com>
2020-05-14 18:38:01 -0500

    minor bug fixes, updates readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # weather-dashboard
+
+This is a dynamically-built weather dashboard which allows a user to search for a city, have that search saved, and see current and future weather conditions there.
+
+## Live Site
+
+https://mcraepv.github.io/weather-dashboard/
+
+## Credits
+
+### jQuery
+
+https://jquery.com/
+
+### Bootstrap
+
+https://getbootstrap.com/
+
+### Open Weather API
+
+https://openweathermap.org/api
+
+## Installation
+
+Visit the live site above, or clone my repo to check out my code.
--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
 
   //initial search function
   function weatherSearch() {
-    city = $("#searchBar").val();
+    searchText = $("#searchBar").val();
     addToSidebar();
     addToMain();
   }
@@ -20,20 +20,22 @@ $(document).ready(function () {
     var newList = $("<li>").addClass("list-group-item");
     newList.attr("id", "listItem");
     var searchList = $("#searchList");
-    newList.on("click", function () {
-      addToMain();
-    });
-    newList.text(city);
+    newList.text(searchText);
     searchList.prepend(newList);
+    city = newList.text();
     cities.push(city);
     localStorage.setItem("savedCities", JSON.stringify(cities));
+    newList.on("click", function () {
+      city = $(this).text();
+      addToMain();
+    });
   }
 
   function addToMain() {
     $("#today").empty();
     $("#fwdForecast").empty();
     var todayURL =
-      "http://api.openweathermap.org/data/2.5/weather?q=" +
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
       city +
       "&units=imperial" +
       "&appid=" +
@@ -46,20 +48,26 @@ $(document).ready(function () {
     }).then(function (response) {
       console.log(response);
       var header = $("<h2>");
-      header.addClass("text-center");
       var dateTodayText = "";
+      var todayIconCode = response.weather[0].icon;
+      console.log(todayIconCode);
+      var todayIcon = $("<img>").attr(
+        "src",
+        "https://openweathermap.org/img/wn/" + todayIconCode + "@2x.png"
+      );
+
       header.text(city);
       var temp = $("<p>").text("Temperature: " + response.main.temp + "\xB0F");
-      temp.addClass("text-center", "my-1");
+      temp.addClass("my-1");
       var humid = $("<p>").text("Humidity: " + response.main.humidity + "%");
-      humid.addClass("text-center", "my-1");
+      humid.addClass("my-1");
       var wind = $("<p>").text("Wind Speed: " + response.wind.speed + " MPH");
-      wind.addClass("text-center", "my-1");
-      $("#today").append(header, temp, humid, wind);
+      wind.addClass("my-1");
+      $("#today").append(header, todayIcon, temp, humid, wind);
       var lat = response.coord.lat;
       var lon = response.coord.lon;
       var uvURL =
-        "http://api.openweathermap.org/data/2.5/uvi?appid=" +
+        "https://api.openweathermap.org/data/2.5/uvi?appid=" +
         apiKey +
         "&lat=" +
         lat +
@@ -93,11 +101,11 @@ $(document).ready(function () {
           uvText.attr("id", "very-high");
         }
         var uvEl = $("<p>").text("UV Index: ");
-        uvEl.addClass("text-center", "my-1");
+        uvEl.addClass("my-1");
         uvEl.append(uvText);
         $("#today").append(uvEl);
         var fiveDayURL =
-          "http://api.openweathermap.org/data/2.5/forecast?q=" +
+          "https://api.openweathermap.org/data/2.5/forecast?q=" +
           city +
           "&units=imperial" +
           "&appid=" +
@@ -123,7 +131,12 @@ $(document).ready(function () {
             var humidEl = $("<p>").text(
               "Humidity: " + thisFWD.main.humidity + "%"
             );
-            cardBody.append(fwdDateEl, tempEl, humidEl);
+            var fwdIconCode = thisFWD.weather[0].icon;
+            var fwdIcon = $("<img>").attr(
+              "src",
+              "https://openweathermap.org/img/wn/" + fwdIconCode + "@2x.png"
+            );
+            cardBody.append(fwdDateEl, fwdIcon, tempEl, humidEl);
             fwdCard.append(cardBody);
             var col = $("<div>");
             col.append(fwdCard);
@@ -133,10 +146,14 @@ $(document).ready(function () {
       });
     });
   }
+
+  //adds saved list items to page on load
   function listItems() {
     var savedCities = JSON.parse(localStorage.getItem("savedCities"));
     if (savedCities !== null) {
       cities = savedCities;
+      city = cities[cities.length - 1];
+      addToMain();
     }
     for (var i = 0; i < cities.length; i++) {
       var newList = $("<li>").addClass("list-group-item");
@@ -145,7 +162,7 @@ $(document).ready(function () {
       newList.text(cities[i]);
       searchList.prepend(newList);
       newList.on("click", function () {
-        city = newList.text();
+        city = $(this).text();
         addToMain();
       });
     }
